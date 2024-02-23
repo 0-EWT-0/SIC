32aa36aa34436650b354bcad80ad71a9a9daa0fa
0-EWT-0 <iamewt@gmail.com>
2024-02-23 10:30:28 -0500

    Cambios de SIC

--- resources/views/Formulario.blade.php
+++ resources/views/Formulario.blade.php
@@ -3,15 +3,16 @@
  Agregar estudiantes
 @endsection
 @section('contenido')
- <form action="{{url('alumnos')}}" method="POST">
+ <form action="{{url('alumnos')}}" method="POST" class="form_alumnos">
     @csrf
-    <input type="text" name="name_student" placeholder="Nombre" value="{{old('name_student')}}">
+    <h1>Registro</h1>
+    <input class="i_nombre_alumno" type="text" name="name_student" placeholder="Nombre" value="{{old('name_student')}}">
     @error('name_student')
     <div style="color: red;"> {{$message}}</div>
     @enderror
-    <input type="text" name="id_student" placeholder="Matricula">
-    <input type="text" name="email_student" placeholder="Correo">
-    <input type="text" name="password_student" placeholder="Contrasena">
-    <button type="submit">Registrar</button>
+    <input class="i_matricula_alumno" type="text" name="id_student" placeholder="Matricula">
+    <input class="i_correo_alumno" type="text" name="email_student" placeholder="Correo">
+    <input class="i_contrasena_alumno" type="text" name="password_student" placeholder="Contrasena">
+    <button class="boton_registro" type="submit">Registrar</button>
  </form>
 @endsection
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -5,15 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/login.css" />
     <title>@yield('titulo')</title>
 </head>
 
+<header>
+    <h1>Bienvenidos</h1>
+</header>
+
 <body>
-    <header>
-        <h1>Bienvenidos</h1>
-    </header>
+
     <main>
+
         @yield('contenido')
+
     </main>
     <footer>Todos los derechos reservados</footer>
 </body>
